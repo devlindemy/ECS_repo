ee265069bf372942d62d9ba4039e2c1bca56555f
Hari25cs <hari25cs@gmail.com>
2020-10-10 04:30:30 +0530

    Create steps.sh

---
+++ steps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -xe
+
+amazon-linux-extras install docker
+sudo yum install docker
+service docker start
+usermod -a -G docker ec2-user
+docker info
+pause 10
+sudo yum install awscli -y
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 782467613200.dkr.ecr.us-east-1.amazonaws.com
+docker build. -t myhtml
+docker tag myhtml:latest 782467613200.dkr.ecr.us-east-1.amazonaws.com/test-repository:latest
+docker push 782467613200.dkr.ecr.us-east-1.amazonaws.com/test-repository:latest
