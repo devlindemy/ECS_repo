16f4c701660f82e0bba6c9657b4c60e375dfe238
Hari25cs <hari25cs@gmail.com>
2020-10-11 00:14:23 +0530

    Update steps.sh

--- steps.sh
+++ steps.sh
@@ -5,7 +5,6 @@ sudo yum install docker -y
 service docker start
 usermod -a -G docker ec2-user
 docker info
-pause 10
 sudo yum install awscli -y
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 782467613200.dkr.ecr.us-east-1.amazonaws.com
 docker build. -t myhtml
